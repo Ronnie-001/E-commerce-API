cf2621a5ee544835a8a03bb42aac599e02d5a8d7
Ronnie-001 <ronald.adesida@gmail.com>
2025-03-15 21:33:45 +0000

    Added SecurityContextHolder to UserController

--- src/main/java/com/ronapps/ecommerceapi/user/UserController.java
+++ src/main/java/com/ronapps/ecommerceapi/user/UserController.java
@@ -2,24 +2,30 @@ package com.ronapps.ecommerceapi.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
+    private final MyUserDetailsService myUserDetailsService;
+    
+    // Use this to persist user login across different endpoints
+    private final SecurityContextRepository securityContextRepository;  
 
 
     @Autowired
-    public UserController(UserService userService, UserRepository userRepository) {
+    public UserController(UserService userService, MyUserDetailsService myUserDetailsService, SecurityContextRepository securityContextRepository) {
         this.userService = userService;
-        this.userRepository = userRepository;
+        this.myUserDetailsService = myUserDetailsService;
+        this.securityContextRepository = securityContextRepository;
+
     }
 
     @GetMapping("/home")
     public String greeting() {
-        return "client needs to create a user first to purchase products";
+        return "User needs to create a user first to purchase products";
     }
     
     @PostMapping("/register_user")
@@ -34,13 +40,18 @@ public class UserController {
         return userDto;
     }
 
-    @GetMapping("/login")
-    public ResponseEntity<String> login(String username, String password) {
-        User user = userRepository.findByUsername(username);
-        if (user != null) {
+    @PostMapping("/login")
+    public ResponseEntity<String> login(String username, String password) {  
+        if (myUserDetailsService.loadUserByUsername(username) != null &&
+            myUserDetailsService.loadUserByUsername(username).getPassword().equals(password)) {
             return ResponseEntity.ok("Login sucsessfull!");
         } else {
             return ResponseEntity.ok("User doesn't exist.");
         }
+    }
+
+    @GetMapping()
+    public String logout() {
+        return "This is the logout endpoint";
     }
 }
